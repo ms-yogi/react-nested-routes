fd032d9e6eb1b6d6cfc6f1338a7048876419082d
ms-yogi <bende.yogini@gmail.com>
2021-06-21 19:49:25 +0530

    Added 404 page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Switch, Route } from 'react-router-dom';
+import NotFound from './NotFound';
 import routes from './Routes';
 import RouteWithSubRoutes from './utils/RouteWithSubRoutes';
 
@@ -10,6 +11,7 @@ function App() {
 				{routes.map((route, i) => (
 					<RouteWithSubRoutes key={i} {...route} />
 				))}
+				<Route component={NotFound} />
 			</Switch>
 		</BrowserRouter>
 	);
---
+++ src/NotFound.js
@@ -0,0 +1,15 @@
+const NotFound = () => {
+	return (
+		<>
+			<div className='login'>
+				<h1 className='text-center'>
+					😰
+					<br />
+					404 Not Found
+				</h1>
+			</div>
+		</>
+	);
+};
+
+export default NotFound;
